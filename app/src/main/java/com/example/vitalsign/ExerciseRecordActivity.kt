376547b3ce70270262626eb2a59e66b3c6971c48
JamesLee <james07297@gmail.com>
2023-12-02 14:31:24 +0900

    pip beta 1.0

--- app/src/main/java/com/example/vitalsign/ExerciseRecordActivity.kt
+++ app/src/main/java/com/example/vitalsign/ExerciseRecordActivity.kt
@@ -1,17 +1,81 @@
 package com.example.vitalsign
 
+import android.app.PictureInPictureParams
+import android.content.res.Configuration
+import android.os.Build
 import android.os.Bundle
 import android.os.CountDownTimer
 import android.os.Handler
+import android.util.Rational
+import android.view.View
 import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
+import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Observer
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.vitalsign.data.Exercise
 import com.example.vitalsign.data.ExerciseSet
 import com.example.vitalsign.data.Routine
+
+
+import androidx.lifecycle.ViewModel
+import androidx.activity.viewModels
+
+class ExerciseRecordViewModel : ViewModel() {
+    // LiveData로 선언
+    val exerciseName = MutableLiveData<String>()
+    val restTimeInSeconds = MutableLiveData<Int>()
+    val secondsElapsed = MutableLiveData<Int>()
+
+    val currentExerciseIndex = MutableLiveData<Int>()
+
+    private var timerHandler = Handler()
+    private lateinit var timerRunnable: Runnable
+
+    init {
+        exerciseName.value = "운동 이름"
+        restTimeInSeconds.value = 60
+        secondsElapsed.value = 0
+
+        currentExerciseIndex.value = 0
+
+        startTimer() // ViewModel 초기화 시 타이머 시작
+    }
+
+    fun setCurrentExercise(exercise: Exercise) {
+        exerciseName.value = exercise.name
+    }
+
+    fun setCurrentExerciseIndex(index: Int) {
+        currentExerciseIndex.value = index
+        // 운동 정보 업데이트 로직도 여기에 추가할 수 있습니다.
+        //exerciseName.value = // 운동 이름 업데이트
+        // 다른 필요한 업데이트 로직
+    }
+
+    fun startTimer() {
+        timerRunnable = object : Runnable {
+            override fun run() {
+                val newTime = (secondsElapsed.value ?: 0) + 1
+                secondsElapsed.postValue(newTime)
+                timerHandler.postDelayed(this, 1000)
+            }
+        }
+        timerHandler.post(timerRunnable)
+    }
+
+    fun stopTimer() {
+        timerHandler.removeCallbacks(timerRunnable)
+    }
+
+    // 필요한 다른 메서드 추가
+}
+
+
 
 //운동 기록 화면
 class ExerciseRecordActivity : AppCompatActivity() {
@@ -28,9 +92,34 @@ class ExerciseRecordActivity : AppCompatActivity() {
     private var currentExerciseIndex = 0 // 현재 운동 인덱스
     private var routineExercises: List<Exercise>? = null
 
+    private val viewModel: ExerciseRecordViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_exercise_record)
+
+        // ViewModel의 LiveData 관찰하여 UI 업데이트
+        viewModel.exerciseName.observe(this, Observer { name ->
+            exerciseTitle.text = name
+        })
+
+        viewModel.restTimeInSeconds.observe(this, Observer { time ->
+            tvRestTime.text = "$time 초"
+        })
+
+        viewModel.secondsElapsed.observe(this, Observer { time ->
+            val minutes = time / 60
+            val seconds = time % 60
+            tvTimer.text = String.format("%02d:%02d", minutes, seconds)
+        })
+
+        viewModel.currentExerciseIndex.observe(this, Observer { index ->
+            currentExerciseIndex = index
+            updateExercise(routineExercises?.getOrNull(index))
+        })
+
+        // 타이머 시작
+        viewModel.startTimer()
 
         val routine = intent.getSerializableExtra("ROUTINE_DATA") as? Routine
         routineExercises = routine?.exercises
@@ -168,18 +257,30 @@ class ExerciseRecordActivity : AppCompatActivity() {
     }
 
 
+    private fun updateTimerDisplay(time: Int) {
+        val minutes = time / 60
+        val seconds = time % 60
+        tvTimer.text = String.format("%02d:%02d", minutes, seconds)
+    }
+
     override fun onPause() {
         super.onPause()
-        timerHandler.removeCallbacks(timerRunnable)
+        if (!isInPictureInPictureMode) {
+            viewModel.stopTimer()
+        }
+        //timerHandler.removeCallbacks(timerRunnable)
     }
 
     override fun onResume() {
         super.onResume()
+        if (!isInPictureInPictureMode) {
+            viewModel.startTimer()
+        }
         // 기존 타이머를 중지하고 다시 시작
-        if (this::timerHandler.isInitialized) {
+        /*if (this::timerHandler.isInitialized) {
             timerHandler.removeCallbacks(timerRunnable)
         }
-        startTimer()
+        startTimer()*/
     }
 
 
@@ -190,7 +291,12 @@ class ExerciseRecordActivity : AppCompatActivity() {
 
     private fun moveToNextExercise() {
         // 다음 인덱스로 이동
-        currentExerciseIndex++
+       /* currentExerciseIndex++
+
+        val nextExercise = routineExercises?.getOrNull(currentExerciseIndex)
+        nextExercise?.let {
+            viewModel.setCurrentExercise(it)
+        }
 
         // 다음 운동이 리스트에 존재하는지 확인하고 업데이트
         if (currentExerciseIndex < (routineExercises?.size ?: 0)) {
@@ -199,6 +305,18 @@ class ExerciseRecordActivity : AppCompatActivity() {
             // 다음 운동이 없는 경우 처리 (예: 화면 종료 또는 메시지 표시)
             Toast.makeText(this, "모든 운동을 완료했습니다.", Toast.LENGTH_SHORT).show()
             stopTimer()
+            finish()
+        }*/
+        val nextIndex = (viewModel.currentExerciseIndex.value ?: 0) + 1
+        if (nextIndex < (routineExercises?.size ?: 0)) {
+            viewModel.setCurrentExerciseIndex(nextIndex)
+            val nextExercise = routineExercises?.getOrNull(nextIndex)
+            nextExercise?.let {
+                viewModel.setCurrentExercise(it)
+            }
+        } else {
+            Toast.makeText(this, "모든 운동을 완료했습니다.", Toast.LENGTH_SHORT).show()
+            viewModel.stopTimer()
             finish()
         }
     }
@@ -222,6 +340,109 @@ class ExerciseRecordActivity : AppCompatActivity() {
                     isCompleted = false     // 완료 여부
                 )
             }
+            setAdapter.setData(setsData.toMutableList())
+        }
+    }
+
+
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    override fun onUserLeaveHint() {
+        val params = PictureInPictureParams.Builder()
+            .setAspectRatio(Rational(16, 9)) // 비율 설정
+            .build()
+        enterPictureInPictureMode(params)
+    }
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    override fun onPictureInPictureModeChanged(isInPictureInPictureMode: Boolean, newConfig: Configuration) {
+        super.onPictureInPictureModeChanged(isInPictureInPictureMode, newConfig)
+        if (isInPictureInPictureMode) {
+            // PiP 모드로 전환 시 PiP 전용 레이아웃 사용
+            setContentView(R.layout.activity_exercise_record_pip)
+            // PiP 레이아웃의 뷰 바인딩
+            bindPipViews()
+            //viewModel.startTimer() // PiP 모드에서 타이머 재개
+        } else {
+            // 일반 모드로 전환 시 원래의 레이아웃 사용
+            setContentView(R.layout.activity_exercise_record)
+            // 기존 레이아웃의 뷰 바인딩
+            bindRegularViews()
+            //viewModel.startTimer() // 일반 모드에서 타이머 재개
+            refreshRecyclerView()
+        }
+    }
+
+    private fun bindPipViews() {
+        // PiP 모드 뷰 바인딩
+        val tvPipTimer: TextView = findViewById(R.id.tvPipTimer)
+        val tvPipRestTime: TextView = findViewById(R.id.tvPipRestTime)
+        val tvPipExerciseName: TextView = findViewById(R.id.tvPipExerciseName)
+
+        // ViewModel의 LiveData 관찰
+        viewModel.secondsElapsed.observe(this, Observer { time ->
+            val minutes = time / 60
+            val seconds = time % 60
+            tvPipTimer.text = String.format("%02d:%02d", minutes, seconds)
+        })
+
+        viewModel.restTimeInSeconds.observe(this, Observer { time ->
+            tvPipRestTime.text = "$time 초"
+        })
+
+        viewModel.exerciseName.observe(this, Observer { name ->
+            tvPipExerciseName.text = name
+        })
+
+        tvPipExerciseName.text = viewModel.exerciseName.value
+    }
+
+    private fun bindRegularViews() {
+        // 일반 레이아웃의 뷰 바인딩 및 초기화
+        tvTimer = findViewById(R.id.tvTimer)
+        tvRestTime = findViewById(R.id.tvRestTime)
+        exerciseTitle = findViewById(R.id.exerciseTitle)
+        setRecyclerView = findViewById(R.id.recyclerViewExerciseSets)
+
+        // RecyclerView 및 어댑터 설정
+        setAdapter = ExerciseRecordSetAdapter(mutableListOf())
+        setRecyclerView.layoutManager = LinearLayoutManager(this)
+        setRecyclerView.adapter = setAdapter
+
+        // ViewModel의 LiveData 관찰하여 UI 업데이트
+        viewModel.exerciseName.observe(this, Observer { name ->
+            exerciseTitle.text = name
+        })
+
+        viewModel.restTimeInSeconds.observe(this, Observer { time ->
+            tvRestTime.text = "$time 초"
+        })
+
+        viewModel.secondsElapsed.observe(this, Observer { time ->
+            val minutes = time / 60
+            val seconds = time % 60
+            tvTimer.text = String.format("%02d:%02d", minutes, seconds)
+        })
+
+        // 리사이클러뷰 및 기타 뷰 초기화
+        // ... 여기에 리사이클러뷰 및 기타 필요한 뷰의 초기화 로직을 추가합니다.
+
+        refreshRecyclerView()
+    }
+    private fun refreshRecyclerView() {
+        // 현재 선택된 운동을 가져옵니다.
+        val currentExercise = routineExercises?.getOrNull(currentExerciseIndex)
+        currentExercise?.let { exercise ->
+            // 현재 운동의 세트 데이터를 생성합니다.
+            val setsData = List(exercise.sets) { index ->
+                ExerciseSet(
+                    setNumber = index + 1,
+                    weight = exercise.weight,
+                    repetitions = exercise.repetitions,
+                    isCompleted = false
+                )
+            }
+            // RecyclerView 어댑터에 데이터를 설정합니다.
             setAdapter.setData(setsData.toMutableList())
         }
     }
