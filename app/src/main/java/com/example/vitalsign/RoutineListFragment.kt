dcb352b4645c8d8f1afd3b32a9d1f777d45c3ecc
Detectheif <hbj970417@naver.com>
2023-12-02 19:16:57 +0900

    기능 추가 및 오류 수정 운동편집화면에 데이터가 있는 경우 정상적으로 인텐트 처리하게끔 수정 운동 아이템 디자인 살짝 수정 루틴리스트에 초기 데이터 삽입 운동편집화면 운동이름 수정 가능한 상태로 변경 그래들에 뷰모델 관련 디펜던시 추가

--- app/src/main/java/com/example/vitalsign/RoutineListFragment.kt
+++ app/src/main/java/com/example/vitalsign/RoutineListFragment.kt
@@ -69,11 +69,75 @@ class RoutineListFragment : Fragment() {
 
     private fun loadRoutines() {
         // 예시 데이터 로딩
+//        val routines = listOf(
+//            Routine("1", "Routine 1", "", mutableListOf(Exercise("Exercise 1", 3, 10.0, 10), Exercise("Exercise 3", 3, 10.0, 10))),
+//            Routine("2", "Routine 2", "", mutableListOf(Exercise("Exercise 2", 4, 12.5, 8)))
+//        )
+
         val routines = listOf(
-            Routine("1", "Routine 1", "", mutableListOf(Exercise("Exercise 1", 3, 10.0, 10), Exercise("Exercise 3", 3, 10.0, 10))),
-            Routine("2", "Routine 2", "", mutableListOf(Exercise("Exercise 2", 4, 12.5, 8)))
-            // 추가 루틴...
+            Routine(
+                "1",
+                "상체 루틴",
+                "",
+                mutableListOf(
+                    Exercise("벤치 프레스", 4, 75.0, 10),
+                    Exercise("덤벨 플라이", 3, 20.0, 12),
+                    Exercise("랫 풀 다운", 4, 60.0, 10),
+                    Exercise("숄더 프레스", 4, 50.0, 10),
+                    Exercise("바이셉스 컬", 3, 15.0, 12)
+                )
+            ),
+            Routine(
+                "2",
+                "하체 루틴",
+                "",
+                mutableListOf(
+                    Exercise("스쿼트", 4, 100.0, 10),
+                    Exercise("데드리프트", 3, 120.0, 8),
+                    Exercise("레그 프레스", 4, 150.0, 10),
+                    Exercise("레그 컬", 3, 40.0, 12),
+                    Exercise("캐프 레이즈", 4, 30.0, 15)
+                )
+            ),
+            Routine(
+                "3",
+                "전신 루틴",
+                "",
+                mutableListOf(
+                    Exercise("풀업", 3, 0.0, 10),
+                    Exercise("푸시업", 4, 0.0, 15),
+                    Exercise("런지", 3, 20.0, 12),
+                    Exercise("플랭크", 3, 0.0, 60), // 시간은 초 단위
+                    Exercise("버피 테스트", 2, 0.0, 15)
+                )
+            ),
+            Routine(
+                "4",
+                "코어 강화 루틴",
+                "",
+                mutableListOf(
+                    Exercise("크런치", 3, 0.0, 15),
+                    Exercise("러시안 트위스트", 3, 0.0, 20),
+                    Exercise("플랭크", 3, 0.0, 60), // 시간은 초 단위
+                    Exercise("마운틴 클라이머", 3, 0.0, 20),
+                    Exercise("레그 레이즈", 3, 0.0, 12)
+                )
+            ),
+            Routine(
+                "5",
+                "카디오 및 지구력 루틴",
+                "",
+                mutableListOf(
+                    Exercise("조깅", 1, 0.0, 900), // 시간은 초 단위, 15분
+                    Exercise("자전거 타기", 1, 0.0, 600), // 시간은 초 단위, 10분
+                    Exercise("스킵 로프", 3, 0.0, 120), // 시간은 초 단위, 2분
+                    Exercise("스텝업", 3, 0.0, 20),
+                    Exercise("버피", 3, 0.0, 15)
+                )
+            )
         )
+
+
          routineAdapter.updateData(routines)
         // TODO: 루틴 데이터 로딩 로직 구현
     }
